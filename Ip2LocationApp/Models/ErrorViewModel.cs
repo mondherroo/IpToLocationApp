b47f659d6c33580c23ab48c9a53a742f7d659818
mondherroo <mondheriron6@gmail.com>
2020-02-16 01:05:56 -0800

    Add project files.

---
+++ Ip2LocationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ip2LocationApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
